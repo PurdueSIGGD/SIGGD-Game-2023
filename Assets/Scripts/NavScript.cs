5b4154839ac87bf390c2a5434a83e07ab4468f1f
Jordan Hindes <111909489+Jhindes@users.noreply.github.com>
2023-09-14 00:16:36 -0400

    Implemented Pathfinding

--- Assets/Scripts/NavScript.cs
+++ Assets/Scripts/NavScript.cs
@@ -1,18 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public NavMeshAgent agent;
+    public GameObject otherObject;
 
     // Update is called once per frame
     void Update()
     {
-        
+        agent.SetDestination(otherObject.transform.position);
     }
 }
