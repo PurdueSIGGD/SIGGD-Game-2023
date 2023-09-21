29d7d2c53ec1a811d1c1a33698bfeba1c7e997db
DinoSage <dinosage5632@gmail.com>
2023-09-20 21:48:23 -0400

    Basic Turret Shooting Implemented

---
+++ Assets/Scripts/Shooter.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+
+    // Serialize Field
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] Transform bulletSpawnPoint;
+    [SerializeField] float bulletSpeed = 10;
+    [SerializeField] float fireRate = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Fire", 1, fireRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void Fire()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = (bulletSpawnPoint.forward * 10 );
+        bullet.AddComponent<Bullet>();
+    }
+
+}
