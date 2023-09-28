03e12a27bbdbb5521aacf73454c24089150b5f8a
Kyle <45471789+kphillips2003@users.noreply.github.com>
2023-09-27 20:13:58 -0400

    generator script adds charge

---
+++ Assets/Scripts/generator.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generator : MonoBehaviour
+{
+    int capactity = 10000;
+    int charge = 0;
+    int increment_val = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (charge < capactity) {
+            charge += increment_val;
+            if (charge > capactity) {
+                charge = capactity;
+            }
+        }
+        // if (charge % 100 == 0) {
+        //     UnityEngine.Debug.Log("Charge: " + charge);
+        // }
+    }
+}
