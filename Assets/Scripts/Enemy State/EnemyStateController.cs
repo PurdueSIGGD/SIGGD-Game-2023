5e189063cb81992862320a2c0e22523ad9b0bc04
Lance <lance.salvad@gmail.com>
2023-10-11 20:08:37 -0400

    Updated commenting

--- Assets/Scripts/Enemy State/EnemyStateController.cs
+++ Assets/Scripts/Enemy State/EnemyStateController.cs
@@ -9,7 +9,6 @@ public class EnemyStateController : MonoBehaviour
     public Dictionary<EnemyState, int> weightTable; // Table of probability weights for each state,
                                                     // should be instantiated by subclasses of this class.
     public EnemyState currentState;
-    private EnemyState nextstate; // Reserved for Tick()
 
     void Start()
     {
@@ -23,16 +22,17 @@ public class EnemyStateController : MonoBehaviour
     }
 
     // Updates at intervals slower than every frame; Used to update state
-    IEnumerator Tick()
+    IEnumerator Tick() //TODO change to method called by a synchronized tick method??
     {
+        EnemyState nextState;
         while (true)
         {
             yield return new WaitForSeconds(0.25f);
-        
+
+            nextState = currentState; // Randomly select next state based on weighted probabilities
             bool shouldSwitch = currentState.StateTick(); // Tick will return a bool for whether a switch should be attempted
             if (!shouldSwitch) yield return null;
 
-            EnemyState nextState = currentState; // Calculate next state based on weighted probabilities
             int weightSum = 0;
             foreach (KeyValuePair<EnemyState, int> state in weightTable)
             {
@@ -49,12 +49,12 @@ public class EnemyStateController : MonoBehaviour
                 }
             }
 
-            if (nextstate != currentState) SwitchState(nextstate);
+            if (nextState != currentState) SwitchState(nextState);
         }
     }
 
     // Stops the current state and begins the new state specified by argument.
-    // Should be called from Start() or coroutine for finite states (e.g. attacks)
+    // Should be called from coroutine for finite states (e.g. attacks). (Beware of recursive calls!)
     // and from Tick() for non-finite states (e.g. idle, patrol)
     public void SwitchState(EnemyState nextState)
     {
