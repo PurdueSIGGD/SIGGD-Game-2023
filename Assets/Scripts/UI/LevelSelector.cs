a83163d9614834972a0725a83d5d1cceca91dd14
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2023-10-13 20:39:23 -0400

    add title to level select

--- Assets/Scripts/UI/LevelSelector.cs
+++ Assets/Scripts/UI/LevelSelector.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -15,9 +16,11 @@ public class LevelSelector : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        var levelIdx = 1;
         foreach (var scene in scenes) {
             var sceneButton = Instantiate(selectorObj, transform);
-            sceneButton.GetComponent<Button>().onClick.AddListener(() => print(scene));
+            sceneButton.GetComponentInChildren<TextMeshProUGUI>().text = "Level " + levelIdx++;
+            sceneButton.GetComponent<Button>().onClick.AddListener(() => SceneManager.LoadScene(scene));
         }
     }
 
