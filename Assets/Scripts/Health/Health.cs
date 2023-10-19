38d93d8502aea827248e2632d1fc2b58ab9f5e47
lsalvad <lsalvad@purdue.edu>
2023-10-18 23:12:51 -0400

    Renamed health component name, comments

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -10,7 +10,7 @@ public class EnemyHealth : MonoBehaviour
     [SerializeField]
     private int currHealth;
 
-    public delegate void OnDamage(float damageAmount); // Components dependent on enemy taking damage should subscribe to this delegate
+    public delegate void OnDamage(float damageAmount); // Components dependent on entity taking damage should subscribe to this delegate. To tell an entity it has been damaged, call ProcessDamage().
     private event OnDamage RegisterDamageEvent;
 
     // Start is called before the first frame update
@@ -28,7 +28,8 @@ public class EnemyHealth : MonoBehaviour
         }
     }
 
-    // Method to be called when an enemy is hit. Damage is processed, and will invoke damaged event if successful
+    // TODO: Tell method which entity caused the damage, implement damage types/resistances(???)
+    // Method to be called when an entity is hit. Damage is processed, and will invoke damaged event if damage is successful.
     public void ProcessDamage(float damage)
     {
         if (isInvulnerable) return;
