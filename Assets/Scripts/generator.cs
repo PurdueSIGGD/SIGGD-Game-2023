95f623790c659e015c1670725641859465622770
Kyle <45471789+kphillips2003@users.noreply.github.com>
2023-10-20 13:52:35 -0400

    Rename

--- Assets/Scripts/generator.cs
+++ Assets/Scripts/generator.cs
@@ -4,7 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class generator : MonoBehaviour
+public class Generator : MonoBehaviour
 {
     int capactity = 10000;
     int charge = 0;
