fd8f82caf5417fd38cc273416cfe4c21253a84a8
Jared Brown <57924789+JaredTheBro@users.noreply.github.com>
2023-10-25 18:00:10 -0400

    Support for empty weapon slots

--- Assets/Scripts/PlayerCombat.cs
+++ Assets/Scripts/PlayerCombat.cs
@@ -11,51 +11,62 @@ public class PlayerCombat : MonoBehaviour
     private IWeapon currentWeaponScript;
     private int currentWeaponIndex;
 
-    // Start is called before the first frame update
+    // Initializes weaponObjects array and other variables.
     void Start()
     {
         // Create weapon objects from prefabs
         weaponObjects = new GameObject[weaponPrefabs.Length];
         for (int i = 0; i < weaponPrefabs.Length; i++) {
-            GameObject weapon = Instantiate(weaponPrefabs[i]);
-            weapon.transform.parent = this.transform;
-            weaponObjects[i] = weapon;
-            weapon.GetComponent<IWeapon>().SetEnabled(false);
+            if (weaponPrefabs[i] != null) {
+                GameObject weapon = Instantiate(weaponPrefabs[i]);
+                weapon.transform.parent = this.transform;
+                weaponObjects[i] = weapon;
+                weapon.GetComponent<IWeapon>().SetEnabled(false);
+            }
         }
 
-        if (weaponObjects.Length > 0) {
-            currentWeapon = weaponObjects[0];
-            currentWeaponIndex = 0;
+        // If no prefabs were provided, make a single null entry so the script works
+        if (weaponObjects.Length == 0) {
+            weaponObjects = new GameObject[1];
+        }
+
+        // Set the current weapon to be the first one in the list (even if null)
+        currentWeapon = weaponObjects[0];
+        currentWeaponIndex = 0;
+        if (currentWeapon != null) {
             currentWeaponScript = currentWeapon.GetComponent<IWeapon>();
-            this.EnableWeapon();
         }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Enables the current weapon.
     public void EnableWeapon() {
-        currentWeaponScript.SetEnabled(true);
+        currentWeaponScript?.SetEnabled(true); // ? is null check
     }
 
+    // Disables the current weapon.
     public void DisableWeapon() {
-        currentWeaponScript.SetEnabled(false);
+        currentWeaponScript?.SetEnabled(false); // ? is null check
     }
 
+    // Sets the current weapon to be weaponObjects[index], enables it, and disables the previous weapon.
     public bool ChangeWeapon(int index) {
+        if (currentWeapon != null) {
+            this.DisableWeapon();
+        }
         if ((index >= 0) && (index < weaponPrefabs.Length)) {
-            DisableWeapon();
             currentWeapon = weaponObjects[index];
             currentWeaponIndex = index;
-            currentWeaponScript = currentWeapon.GetComponent<IWeapon>();
-            EnableWeapon();
+            if (currentWeapon != null) {
+                currentWeaponScript = currentWeapon.GetComponent<IWeapon>();
+                this.EnableWeapon();
+            }
+            return true;
         }
         return false;
     }
 
+    // Sets the current weapon to be the provided GameObject if it's in the weaponObjects array,
+    // enables it, and disables the previous weapon.
     public bool ChangeWeapon(GameObject weapon) {
         // Check if list contains the weapon object specified, and switch to it
         for (int i = 0; i < weaponObjects.Length; i++) {
@@ -67,14 +78,18 @@ public class PlayerCombat : MonoBehaviour
         return false;
     }
 
+    // Triggered by primary action button.
     void OnAttack1() {
-        currentWeaponScript.PerformAttack(1);
+        currentWeaponScript?.PerformAttack(1); // ? is null check
     }
 
+    // Triggered by secondary action button.
     void OnAttack2() {
-        currentWeaponScript.PerformAttack(2);
+        currentWeaponScript?.PerformAttack(2); // ? is null check
     }
 
+    // Triggered by the scroll wheel. This changes the weapon to the next or previous one in the
+    // weaponObjects array, wrapping to the opposite end of the array if needed.
     void OnChangeItem(InputValue val) {
         if (val.Get<float>() > 0) {
             if (currentWeaponIndex == weaponObjects.Length - 1) {
