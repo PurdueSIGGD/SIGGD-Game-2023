230e837176ed853a184b435ada68d379030977ca
Lance <lance.salvad@gmail.com>
2023-10-25 20:25:38 -0400

    Blueprint of enemy targeting

--- Assets/Scripts/Enemy Targeting/EnemyTargetingController.cs
+++ Assets/Scripts/Enemy Targeting/EnemyTargetingController.cs
@@ -6,33 +6,82 @@ using UnityEngine.UIElements;
 
 public class EnemyTargeting : MonoBehaviour
 {
-    private struct _weight
-    {
-        float distanceWeight;
-        float 
-    }
-
     public Dictionary<GameObject, _weight> targetWeights;
+    public struct _weight
+    {
+        public float proximityWeight;  // Portion of weight determined by distance to object.
+                                       // Ranges from 0.0-1.0f based on normalized range from player.
+                                       // 0.0f if exceeds aggroRange.
+
+        public float visibilityFactor; // On every tick, determine if target is currently visible(raycast). If visible, refresh this value to 1.0f.
+                                       // For every tick that target is not visible, decay this value by visibilityDecayRate.
+                                       // If visibilityFactor reaches 0, do not select the target
+    }
 
     public GameObject currentTarget;
     public GameObject nextTarget;
     public bool canSwitch;
 
     public float aggroRange;
+    public float visibilityDecayRate;
 
-    void Awake()
+    public virtual void AddTarget(GameObject newTarget)
     {
+        _weight newTargetWeight = new _weight();
+        
+        newTargetWeight.proximityWeight = 0.0f;
+        newTargetWeight.visibilityFactor = 0.0f;
 
+        targetWeights.Add(newTarget, newTargetWeight);
     }
-
-    void Start()
+    public virtual void RemoveTarget(GameObject target)
     {
-        canSwitch = true;
+        targetWeights.Remove(target);
+        UpdateWeights();
+        UpdateTarget();
     }
-
-    void Update()
+    public virtual void UpdateWeights()
     {
+        float distance;
+        _weight targetWeight;
+        foreach (KeyValuePair<GameObject, _weight> targetWeightPair in targetWeights)
+        {
+            targetWeight = targetWeightPair.Value;
 
+            if (true) // In complete code this will check for visibility (e.g. raycast to target)
+            {
+                targetWeight.visibilityFactor = 1.0f;
+            }
+            else
+            {
+                targetWeight.visibilityFactor -= visibilityDecayRate;
+                if (targetWeight.visibilityFactor < 0.0f) targetWeight.visibilityFactor = 0.0f;
+            }
+
+            distance = Vector3.Distance(gameObject.transform.position, targetWeightPair.Key.transform.position);
+            if (distance < aggroRange)
+            {
+                targetWeight.proximityWeight = (aggroRange - distance) / aggroRange;
+            }
+            else
+            {
+                targetWeight.proximityWeight = 0.0f;
+            }
+        }
+    }
+    public virtual void UpdateTarget()
+    {
+        float maxWeight = 0.0f;
+        foreach (KeyValuePair<GameObject, _weight> targetWeightPair in targetWeights)
+        {
+            if (targetWeightPair.Value.visibilityFactor <= 0.0f) continue;
+            if (targetWeightPair.Value.proximityWeight > maxWeight)
+            {
+                nextTarget = targetWeightPair.Key;
+                maxWeight = targetWeightPair.Value.proximityWeight;
+            }
+        }
+        if (canSwitch) currentTarget = nextTarget;
     }
 
     IEnumerator Tick()
@@ -40,28 +89,8 @@ public class EnemyTargeting : MonoBehaviour
         while (true)
         {
             yield return new WaitForSeconds(0.25f);
-
-            // Calculate weights (In final implementation this probably should be implemented by subclasses rather than parent class
-            // so different enemies exhibit different behaviour)
-
-            // Portion of target weight based on proximity.
-            // Ranges from 0 < x < 100 normalized for aggro range, (Percent of aggro distance from enemy)
-            foreach (KeyValuePair<GameObject, float> targetDistance in targetDistances) 
-            {
-                float distance = Vector3.Distance(this.gameObject.transform.position, targetDistance.Key.transform.position);
-                if (distance > aggroRange) continue;
-
-                targetDistance.Value = 100f * (aggroRange - distance) / aggroRange;
-            }
-
-
-
-            float maxPriority = 0;
-            float currentWeight;
-            foreach (GameObject target in targets)
-            {
- 
-            }
+            UpdateWeights();
+            UpdateTarget();
         }
     }
 }
