925a4d2edd62091623dc8efb707e7495d6cd1ef0
AidanV0908 <aidan.velleca@verizon.net>
2023-10-25 21:16:24 -0400

    Created Rocket Turret, Shooter Turret

---
+++ Assets/Scripts/RocketTurret.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RocketTurret : Unit
+{
+    //Rocket turret script
+    //Create function
+    protected override void Create()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //End function
+    protected override void End()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/ShooterTurret.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShooterTurret : Unit
+{
+    //Placeholder turret script
+    //Create function
+    protected override void Create()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    //End function
+    protected override void End()
+    {
+        throw new System.NotImplementedException();
+    }
+}
