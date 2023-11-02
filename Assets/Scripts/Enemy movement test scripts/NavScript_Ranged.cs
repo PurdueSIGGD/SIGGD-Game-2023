8159dffd47d7a0497bb0499fa88bb4a572a75bca
apboezer <aboezer@purdue.edu>
2023-11-01 20:35:01 -0400

    ranged enemies no longer shake

--- Assets/Scripts/Enemy movement test scripts/NavScript_Ranged.cs
+++ Assets/Scripts/Enemy movement test scripts/NavScript_Ranged.cs
@@ -44,10 +44,20 @@ public class NavScript_Ranged : MonoBehaviour
                 for (int i = 0; i < path.corners.Length - 1; i++) {
                     Debug.DrawLine(path.corners[i], path.corners[i + 1], Color.red);
                 }
-                Vector3 move_offset = this_enemy.forward * -0.1f;
-                if ((path.corners[1].Equals(player.position)) || Vector3.Distance(player.position, this_enemy.position) > maxRangeDist) {
+                float distToPlayer = Vector3.Distance(player.position, this_enemy.position);
+                Vector3 move_offset = Vector3.zero;
+
+                
+                if (distToPlayer < (maxRangeDist - 1)) {
+                    Debug.Log("back back");
+                    move_offset = this_enemy.forward * -0.1f;
+                }
+                if (path.corners.Length > 2 || distToPlayer > maxRangeDist) {
+                    Debug.Log("Hello");
                     move_offset = this_enemy.forward;
                 }
+
+
                 RaycastHit leftHit;
                 RaycastHit rightHit;
                 bool leftHitBool = Physics.Raycast((this_enemy.position + (this_enemy.right * -1 * boxSize.x * 0.51f)), (this_enemy.right * -1), out leftHit, rayDist, mask);
@@ -59,8 +69,9 @@ public class NavScript_Ranged : MonoBehaviour
                     move_offset += this_enemy.right * -1;
                 }
                 else if (leftHitBool && rightHitBool) {
-                    move_offset += this_enemy.right * (rightHit.distance- leftHit.distance) * -1;
+                    move_offset += this_enemy.right * (rightHit.distance - leftHit.distance) * -1;
                 }
+                
 
                 RaycastHit frontHit;
                 if (Physics.Raycast((this_enemy.position + (this_enemy.forward * boxSize.z * 0.51f)), this_enemy.forward, out frontHit, rayDist, mask)) {
@@ -79,11 +90,12 @@ public class NavScript_Ranged : MonoBehaviour
                     //Debug.DrawLine(this_enemy.position, (this_enemy.position + flank_offset), Color.white);
                     move_offset += flank_offset;
                 }
-
-
                 move_offset = move_offset.normalized;
+                
                 Debug.DrawLine(this_enemy.position, (this_enemy.position + 2 * move_offset), Color.blue);
-                agent.Move(move_offset * speed * Time.fixedDeltaTime);
+                if (move_offset != Vector3.zero) {
+                    agent.Move(move_offset * speed * Time.fixedDeltaTime);
+                }
             }
         }
 
