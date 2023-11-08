b04d56b46ef4e5a8ee1002ddc858cc13fc7c634e
AidanV0908 <aidan.velleca@verizon.net>
2023-11-08 16:45:52 -0500

    Rotational correction

--- Assets/Scripts/TurretAbstraction/TurretController.cs
+++ Assets/Scripts/TurretAbstraction/TurretController.cs
@@ -11,8 +11,8 @@ using UnityEngine.InputSystem;
  * repeat shooting actions where single use items may not.
  * TODO : Rework Turret Aim to work with this new script
  * TODO : Make sure the TurretController keeps track of turrets and does not exceed turret cap, where 0 = infinite turrets
- * TODO : Add instantiation ( referenced in comment below )
  * TODO : Add in all desired subclasses of turrets
+ * TODO : Turrets in wall
  */
 public class TurretController : MonoBehaviour
 {
@@ -26,8 +26,11 @@ public class TurretController : MonoBehaviour
     [SerializeField] private int TURRET_CAP;
 
     // How far away and close to player turret can be placed (min distance prevents clipping)
-    [SerializeField] private float PLACE_MIN = 3;
-    [SerializeField] private float PLACE_MAX = 5;
+    [SerializeField] private float PLACE_MIN;
+    [SerializeField] private float PLACE_MAX;
+
+    // Maximum placement angle for turrets
+    [SerializeField] private float MAX_PLACE_ANGLE;
 
     // The position of the player
     [HideInInspector] public Vector3 playerPosition;
@@ -68,8 +71,6 @@ public class TurretController : MonoBehaviour
     // Escape key pressed to leave place mode
     public void OnEscape()
     {
-        Debug.Log("Escaping");
-
         // Set place mode to false
         if (placeMode)
         {
@@ -84,7 +85,10 @@ public class TurretController : MonoBehaviour
     void EnterPlaceMode()
     {
         // Check the model
-        (bool valid, Vector3 pos) = CheckValidAndGetPos();
+        bool valid = CheckValid();
+
+        // Get transform
+        (Vector3 pos, Vector3 rot) = GetTransform();
 
         // If position is defined, place on surface
         if (!pos.Equals(Vector3.positiveInfinity))
@@ -113,16 +117,20 @@ public class TurretController : MonoBehaviour
     // Place the turret
     public void PlaceTurret()
     {
-        // Get spawn position and validity
-        (bool valid, Vector3 pos) = CheckValidAndGetPos();
+        // Get validity
+        bool valid = CheckValid();
+
+        // Get spawn location
+        (Vector3 pos, Vector3 rot) = GetTransform();
 
         // Check validity
-        if (valid)
+        if (valid )
         {
             // If the proposed gameobject has a unit behavior, instantiate at position. If not, log warning
             if (unitToSpawn.GetComponent<Unit>() != null)
             {
                 GameObject newUnit = Instantiate(unitToSpawn, pos, Quaternion.identity);
+                newUnit.transform.up = rot;
             }
             else
             {
@@ -145,25 +153,44 @@ public class TurretController : MonoBehaviour
     }
 
     // Returns true if the proposed turret placement position is valid, false otherwise
-    // If the position on a placeable surface exists, it is returned, even if it is too far from the player
-    // If the raycast returns no results, positive infinity is returned for the vector
-    (bool, Vector3) CheckValidAndGetPos()
+    // TODO : Check distance to walls
+    bool CheckValid()
     {
         // Ray from mouse position into screen
         Ray camToWorld = mainCamera.ScreenPointToRay(Input.mousePosition);
 
         // If no placeable surface, return not valid
         if (!Physics.Raycast(camToWorld, out RaycastHit hit, float.PositiveInfinity, LayerMask.GetMask("Placeable")))
-            return (false, Vector3.positiveInfinity);
+            return false;
 
         // If distance too far, return not valid
-        if (!(Vector3.Distance(hit.point, playerPosition) >= PLACE_MIN && Vector3.Distance(hit.point, playerPosition) <= PLACE_MAX))
+        if (!((Vector3.Distance(hit.point, playerPosition) >= PLACE_MIN) && (Vector3.Distance(hit.point, playerPosition) <= PLACE_MAX)))
         {
-            return (false, hit.point);
+            return false;
+        }
+
+        // If hit normal y component is too low (slope too steep), return not valid
+        if (hit.normal.y < Mathf.Cos(Mathf.Deg2Rad * MAX_PLACE_ANGLE))
+        {
+            return false;
         }
 
         // If point is on placeable surface close to player, return valid
-        return (true, hit.point);
+        return true;
+    }
+
+    // Return the position and rotation of raycast hit of mouse
+    (Vector3, Vector3) GetTransform()
+    {
+        // Ray from mouse position into screen
+        Ray camToWorld = mainCamera.ScreenPointToRay(Input.mousePosition);
+
+        // If no placeable surface, return + infinity for position and zero rotation
+        if (!Physics.Raycast(camToWorld, out RaycastHit hit, float.PositiveInfinity, LayerMask.GetMask("Placeable")))
+            return (Vector3.positiveInfinity, Vector3.zero);
+
+        // If point is on placeable surface return the hit point and normal
+        return (hit.point, hit.normal);
     }
 
     // In update, perform place mode updates if canPlace is true
@@ -171,8 +198,11 @@ public class TurretController : MonoBehaviour
     {
         if (placeMode)
         {
-            //Get valid and pos
-            (bool valid, Vector3 pos) = CheckValidAndGetPos();
+            //Get valid
+            bool valid = CheckValid();
+            // Get transform
+            (Vector3 pos, Vector3 rot) = GetTransform();
+
             // If position is infinity, hide GO
             if (pos.Equals(Vector3.positiveInfinity))
             {
@@ -193,8 +223,9 @@ public class TurretController : MonoBehaviour
                     SetColor(blankModel, Color.red);
                 }
 
-                // Reset position
+                // Reset position, rotation
                 blankModel.transform.position = pos;
+                blankModel.transform.up = rot;
             }
         }
     }
