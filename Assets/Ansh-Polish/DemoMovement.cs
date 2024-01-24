c280f668ef47ea995aa1a0a5ac7e71d983fb4f2c
DinoSage <dinosage5632@gmail.com>
2024-01-24 14:14:14 -0500

    Clean Up Movement Script

--- Assets/Ansh-Polish/DemoMovement.cs
+++ Assets/Ansh-Polish/DemoMovement.cs
@@ -24,11 +24,12 @@ public class DemoMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // Get Components
         RB = GetComponent<Rigidbody>();
         data = GetComponent<DemoPlayerData>();
     }
 
-    // Update is called once per frame
+    // Regular Frame Update
     void Update()
     {
         // Poll - Update Player Direction Based on Mouse Position
@@ -43,23 +44,26 @@ public class DemoMovement : MonoBehaviour
     // Input Action (Move) - Update Move Direction
     public void OnMove(InputValue value)
     {
-
         inputDirection = value.Get<Vector2>();
     }
 
     private void move()
     {
+        // Find Input Angle
         Vector3 inputVector = new Vector3(inputDirection.x, 0, inputDirection.y);
         float angle = Vector3.Angle(Vector3.forward, inputVector);
         angle *= (inputVector.x < 0) ? -1 : 1;
 
+        // Apply Input Angle to Camera Face Direction
         Vector3 cameraForward = new Vector3(mainCamera.transform.forward.x, 0, mainCamera.transform.forward.z);
         Vector3 rotated = Quaternion.AngleAxis(angle, Vector3.up) * cameraForward;
-        Vector3 targetSpeed = (rotated * inputVector.magnitude).normalized * data.GetMaxSpeed();
-        RB.velocity = targetSpeed;
+
+        // Calcualte Target Velocity
+        Vector3 targetVelocity = (rotated * inputVector.magnitude).normalized * data.GetMaxSpeed();
+        RB.velocity = targetVelocity;
     }
 
-
+    // Physics Update
     void FixedUpdate()
     {
         move();
