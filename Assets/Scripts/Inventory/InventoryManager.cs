4b5e7868ca82e8311025f9f4697eb1adff019575
Kyle <45471789+kphillips2003@users.noreply.github.com>
2024-02-14 20:28:15 -0500

    Countdowns!!!
    It counts down, nothing else

--- Assets/Scripts/Inventory/InventoryManager.cs
+++ Assets/Scripts/Inventory/InventoryManager.cs
@@ -5,19 +5,19 @@ using UnityEngine.InputSystem;
 
 
 public class InventoryManager : MonoBehaviour {
-    public static InventoryManager instance;
-    private Item[] inventory;
-    private int numOfSlots = 5; // Random Value (Will Change)
+    // public static InventoryManager instance;
+    // private Item[] inventory;
+    // private int numOfSlots = 5; // Random Value (Will Change)
     
     
     // Start is called before the first frame update
     void Start() {
-        inventory = new Item[5];
-        Item[0] = new Item(0, "item 0", "this is the 0th item", 5);
-        Item[1] = new Item(1, "item 1", "this is the 1st item", 5);
-        Item[2] = new Item(2, "item 2", "this is the 2nd item", 5);
-        Item[3] = new Item(3, "item 3", "this is the 3rd item", 5);
-        Item[4] = new Item(4, "item 4", "this is the 4th item", 5);
+        // inventory = new Item[5];
+        // Item[0] = new Item(0, "item 0", "this is the 0th item", 5);
+        // Item[1] = new Item(1, "item 1", "this is the 1st item", 5);
+        // Item[2] = new Item(2, "item 2", "this is the 2nd item", 5);
+        // Item[3] = new Item(3, "item 3", "this is the 3rd item", 5);
+        // Item[4] = new Item(4, "item 4", "this is the 4th item", 5);
 
 
 
--- Assets/Scripts/Inventory/Item.cs
+++ Assets/Scripts/Inventory/Item.cs
@@ -1,19 +1,104 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class Item {
+//     public string title;
+//     public string description;
+//     public Sprite icon;
+//     public int cooldown;
+
+//     public Item(int id, string title, string description, int cooldown) {
+//         this.id = id;
+//         this.title = title;
+//         this.description = description;
+//         this.cooldown = cooldown;
+//     }
+// }
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.InputSystem;
 
-public class Item {
-    public string title;
-    public string description;
-    public Sprite icon;
-    public int cooldown;
+public class Item : MonoBehaviour {
+    [SerializeField] Image border;
+    [SerializeField] Image background;
+    [SerializeField] Image itemImage;
+    [SerializeField] int itemId;
+    [SerializeField] TextMeshProUGUI durationText;
 
-    public Item(int id, string title, string description, int cooldown) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-        this.cooldown = cooldown;
+    // Item Numbers are 1 indexed but the array is 0 indexed
+    private int[] cooldowns = {50, 500, 5, 10, 153};
+    private Sprite[] sprites;
+    private int currentCooldown;
+    void Awake() {
+        currentCooldown = cooldowns[itemNum2ArrayIndex(itemId)];
+        durationText.text = currentCooldown.ToString();
     }
 
+    private int itemNum2ArrayIndex(int ind) {return ind - 1;}
 
+    // Update is called once per frame
+    void Update() {
+        if (currentCooldown != 0) {
+            currentCooldown--; // TODO: Make cooldown in seconds and not frames or find a good frame cooldowns
+            durationText.text = currentCooldown.ToString();
+        }
+
+        if (itemId == 1) {
+            if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1)) {
+                Debug.Log(itemId + "1 Pressed either above the keyboard or on the numpad");
+            
+                currentCooldown = cooldowns[itemNum2ArrayIndex(itemId)];
+                durationText.text = cooldowns[itemNum2ArrayIndex(itemId)].ToString();
+
+            }
+        }
+        else if (itemId == 2) {
+            if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2)) {
+                Debug.Log(itemId + "2 Pressed either above the keyboard or on the numpad");
+            
+                currentCooldown = cooldowns[itemNum2ArrayIndex(itemId)];
+                durationText.text = cooldowns[itemNum2ArrayIndex(itemId)].ToString();
+
+            }
+        }
+        else if (itemId == 3) {
+            if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3)) {
+                Debug.Log(itemId + "3 Pressed either above the keyboard or on the numpad");
+
+                currentCooldown = cooldowns[itemNum2ArrayIndex(itemId)];
+                durationText.text = cooldowns[itemNum2ArrayIndex(itemId)].ToString();
+
+            }
+        }
+        else if (itemId == 4) {
+            if (Input.GetKeyDown(KeyCode.Alpha4) || Input.GetKeyDown(KeyCode.Keypad4)) {
+                Debug.Log(itemId + "4 Pressed either above the keyboard or on the numpad");
+
+                currentCooldown = cooldowns[itemNum2ArrayIndex(itemId)];
+                durationText.text = cooldowns[itemNum2ArrayIndex(itemId)].ToString();
+
+            }
+        }
+        else if (itemId == 5) {
+            if (Input.GetKeyDown(KeyCode.Alpha5) || Input.GetKeyDown(KeyCode.Keypad5)) {
+                Debug.Log(itemId + "5 Pressed either above the keyboard or on the numpad");
+
+                currentCooldown = cooldowns[itemNum2ArrayIndex(itemId)];
+                durationText.text = cooldowns[itemNum2ArrayIndex(itemId)].ToString();
+
+            }
+        }
+        
+        
+        
+        
+    }
+
+    void updateDurationText() {
+
+    }
 }
