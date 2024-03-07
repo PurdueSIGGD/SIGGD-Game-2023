a450ee74bd044f042526ca571435a20576ef0389
apboezer <aboezer@purdue.edu>
2024-03-06 22:11:33 -0500

    edited jelly more

--- Assets/Scripts/Health/JellyHealthPoints.cs
+++ Assets/Scripts/Health/JellyHealthPoints.cs
@@ -7,10 +7,13 @@ public class JellyHealthPoints : HealthPoints
 
     [SerializeField] private GameObject jellyChild;
     [SerializeField] private int childCount;
+    [SerializeField] private float childSpread;
+    [SerializeField] private float spreadTime;
 
     //for debug purposes
     [SerializeField] private int countTime = 1000;
     private int count = 0;
+    private GameObject[] children; 
 
     public void Update() {
         count += 1;
@@ -24,11 +27,26 @@ public class JellyHealthPoints : HealthPoints
 
     public void kill() {
         Debug.Log("kill jelly");
-        for (int i = 0; i < childCount; i++) {
-            Debug.Log("child" + i);
-            Instantiate(jellyChild, this.gameObject.transform.position, Quaternion.identity);
-        }
-        Destroy(this.gameObject);
+        spawnJellies();
+        this.gameObject.GetComponent<MeshRenderer>().enabled = false;
+        this.gameObject.GetComponent<BoxCollider>().enabled = false;
+        StartCoroutine(die());
     }
 
+    private void spawnJellies() {
+        children = new GameObject[childCount];
+        for (int i = 0; i < childCount; i++) {
+            children[i] = Instantiate(jellyChild, this.gameObject.transform.position, Quaternion.identity);
+            children[i].GetComponent<MobNav>().enabled = false;
+        }
+    }
+
+    IEnumerator die() {
+        yield return new WaitForSeconds(spreadTime);
+        for (int i = 0; i < childCount; i++) {
+            children[i].GetComponent<MobNav>().enabled = true;
+        }
+        Destroy(this.gameObject);
+        yield return null;
+    }
 }
