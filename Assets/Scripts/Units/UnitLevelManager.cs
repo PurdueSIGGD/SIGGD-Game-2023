e2dccddbd451690a6eafb134d249d88e2319c307
DinoSage <dinosage5632@gmail.com>
2024-04-20 14:54:00 -0400

    +Unit Families & Unit System Script for Levelling

---
+++ Assets/Scripts/Units/UnitLevelManager.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitLevelManager : MonoBehaviour
+{
+    [SerializeField]
+    UnitFamily[] unitFamilies;
+
+    public void Upgrade()
+    {
+
+    }
+}
