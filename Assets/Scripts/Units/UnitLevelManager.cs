3d92ea559c06bebf7869c53252d81f7bbbbc7da0
DinoSage <dinosage5632@gmail.com>
2024-04-21 15:05:55 -0400

    Turned Unit System into Scriptable Object Unit Config

--- Assets/Scripts/Units/UnitLevelManager.cs
+++ Assets/Scripts/Units/UnitLevelManager.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class UnitLevelManager : MonoBehaviour
+[CreateAssetMenu(fileName = "Unit Config")]
+public class UnitLevelManager : ScriptableObject
 {
     // -- Serialize Fields --
     [SerializeField]
