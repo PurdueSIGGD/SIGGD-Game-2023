43f9ddf124211a2781352d7d9a4c4a50daed7b83
iyang2006 <yang2006@purdue.edu>
2024-07-24 17:40:41 -0600

    edited some prefabs

---
+++ Assets/Scripts/UI/MiniMap.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMap : MonoBehaviour
+{
+    private GameObject radarRing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //radarRing = 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
